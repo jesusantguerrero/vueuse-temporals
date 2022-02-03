6d66e6ebc4150dca612ee45298391411bd6d53c2
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-02-03 08:13:39 -0400

    docs: update docs

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,22 +1,38 @@
 <template>
   <div>
-    <input type="checkbox" :value="toggleable ? 'checked' : 'unchecked'" @change="toggle" />
-    Current value is: {{ toggleable }}
+    <input :value="currentDate" type="date" @change="setDate" />
+    Current value is: {{ selectedDay }}
+    <button @click="controls.previous()"> Prev </button>
+    <div v-for="day in selectedDateRange" :key="day.toISOString()"> 
+      {{ day }}
+    </div>
+    <button @click="controls.next()"> Next </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { useToggle } from '../../src'
+import { computed, defineComponent } from 'vue'
+import { useDatePager } from '../../src'
 
 export default defineComponent({
   name: 'App',
   setup() {
-    const [toggleable, toggle] = useToggle()
+    const { selectedDay, controls, selectedDateRange } = useDatePager({
+      nextMode: 'day'
+    })
 
+    const currentDate = computed(() => selectedDay.value.toISOString().slice(0, 10))
+    const setDate = (e: Event) => {
+      const target = e.target as HTMLInputElement
+      const date = new Date(Date.parse(target.value))
+      controls.setDay(date);
+    }
     return {
-      toggleable,
-      toggle
+      currentDate,
+      setDate,
+      selectedDay,
+      controls,
+      selectedDateRange
     }
   }
 })
